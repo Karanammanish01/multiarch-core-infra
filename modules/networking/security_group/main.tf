3d9052c8010e99b9ecff410c64ea0c9864ab96d1
Manish Reddy Devops <manishkaranam20@gmail.com>
2026-06-25 16:27:03 +0530

    Changes done on security group

--- modules/networking/security_group/main.tf
+++ modules/networking/security_group/main.tf
@@ -19,10 +19,14 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
+
+  description = "Allowing the port 443"
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.this.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
+
+  description = "All ports are allowed"
 }
