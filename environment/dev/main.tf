c23d3c8997ef9ff049cd832ac41e894be478dd52
Manish Reddy Devops <manishkaranam20@gmail.com>
2026-06-25 16:57:42 +0530

    checkov ignore setting

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -1,15 +1,17 @@
-# Checkov:skip=CKV2_AWS_11: "Ensure VPC flow logging is enabled in all VPCs"
+
 
 module "vpc" {
   source = "../../modules/networking/vpc"
-
+  #checkov:skip=CKV2_AWS_11: Flow logs will be implemented later
   region     = var.aws_region
   cidr_block = var.cidr_block
 }
 
 module "security_group" {
+  
   source = "../../modules/networking/security_group"
-
+  #checkov:skip=CKV2_AWS_5: Security group is a reusable module and will be attached by consuming resources
+  
   name       = var.security_group_name
   vpc_id     = module.vpc.vpc_id
   cidr_block = var.cidr_block
